58b8f28384ec55d77a65f8a6811872af067c0404
icbritto <igorbritto.pers@gmail.com>
2024-02-17 01:13:37 -0300

    added some lsp language servers (tsserver, cssls, html, marksman)

--- lua/icbritto/plugins/lsp-config.lua
+++ lua/icbritto/plugins/lsp-config.lua
@@ -9,7 +9,15 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "rust_analyzer", "powershell_es" },
+        ensure_installed = {
+          "lua_ls",
+          "rust_analyzer",
+          "powershell_es",
+          "tsserver",
+          "cssls",
+          "html",
+          "marksman"
+        },
       })
     end
   },
@@ -19,6 +27,10 @@ return {
       require("lspconfig").lua_ls.setup {}
       require("lspconfig").rust_analyzer.setup {}
       require("lspconfig").powershell_es.setup {}
+      require("lspconfig").tsserver.setup {}
+      require("lspconfig").cssls.setup {}
+      require("lspconfig").html.setup {}
+      require("lspconfig").marksman.setup {}
 
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
